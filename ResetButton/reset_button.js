69162b12b1e480ea9bc45f7c4cf31c40f0187833
Dilawaiz <272792933+pringlenvm744-create@users.noreply.github.com>
2026-07-16 13:40:06 -0400

    Added

---
+++ ResetButton/reset_button.js
@@ -0,0 +1,28 @@
+let count = 0;
+let reset = 0;
+
+function increaseCount(){
+    count++;
+    checkCountValue();
+    displayCount();
+}
+
+function checkCountValue() {
+    if (count == 10){
+        alert("Your post has gained 10 followers!")
+    } else if (count == 20){
+        alert("Your post has gained 20 followers!")
+    }
+}
+
+function displayCount(){
+    document.getElementById("countDisplay").innerHTML = count; 
+}
+
+function resetCount(){
+    count = reset;
+    document.getElementById("countDisplay").innerHTML = reset;
+    alert("Your followers are 0!");
+
+}
+
