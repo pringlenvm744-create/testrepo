2c35e19decdc6f0efe71dda9dc223018b333b0ba
Dilawaiz <272792933+pringlenvm744-create@users.noreply.github.com>
2026-07-21 17:23:44 -0400

    added prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$23', 'Oatmeal -$78', 'Frittata -$45'];
+const mainCourseMenu = ['Steak -$34', 'Pasta -$22', 'Burger -$11', 'Salmon -$4'];
+const dessertMenu = ['Cake -$5', 'Ice Cream -$67', 'Pudding -$98', 'Fruit Salad -$29'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item,index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
